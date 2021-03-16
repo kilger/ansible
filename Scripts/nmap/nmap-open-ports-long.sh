99393888198e55e8b87654134df864401235b186
Christopher Kilger <chriskilger+github@gmail.com>
2021-03-16 16:49:04 -0400

    new

---
+++ Scripts/nmap/nmap-open-ports-long.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#title           :nmap-open-ports-long.sh
+#description     :Extract tab delimited table (ip, name, port, service, version) from Nmap XML output
+#author          :Tim Medin @TimMedin tim@redsiege.com
+#date            :20171229
+#version         :1.0
+#usage           :nmap-open-ports-long.sh nmap-results.xml
+#repository      :https://github.com/RedSiege/rstools
+#notes           :Install xmlstarlet. Based on https://gist.github.com/bdpuk/4587372
+
+if [ -z "$1" ]; then
+  echo "You must supply an Nmap xml file"
+  exit
+fi
+
+xmlstarlet sel -T -t -m "//state[@state='open']" -m ../../.. -v 'address[not(@addrtype="mac")]/@addr' -o "	" -m hostnames/hostname -i @name -v @name -b -b -b -o "	" -m .. -v @portid -o '/' -v @protocol -o "	" -m service -v @name -i "@tunnel='ssl'" -o 's' -b -o "	" -v @product -o ' ' -v @version -v @extrainfo -b -n "$1"
